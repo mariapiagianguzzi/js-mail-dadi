2586c59d20118aa2d317c35b1196abb5cc9d7f8b
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2020-11-18 14:08:41 +0100

    mail

--- mail/js/main.js
+++ mail/js/main.js
@@ -5,11 +5,29 @@ stampa un messaggio appropriato sull’esito del controllo. */
 
 
 /* INSERIRE EMAIL */
-var mail = parseInt(prompt("Inserisci la tua Mail"));
+var mail = prompt("inserisci la tua mail?")
+console.log(mail);
 
 /* LISTA DI CHI PUò ACCEDERE */
-var names = ["Pia", "Tom Hiddleston", "Chris Evans", "IronMan", "Sandro Pertini", "Topolino", "Stalin",];
+var names = ["Pia@gmail.com", "TomHiddleston@gmail.com", "ChrisEvans@gmail.com", "IronMan@gmail.com", "Sandro Pertini@gmail.com", "Topolino@gmail.com", "Stalin@gmail.com",];
 document.getElementById("demo").innerHTML = names;
+
+var mailTrovata = false; 
+
+/* VERIFICARE I NOMI */
+for (var i = 0; i < names.length; i++) {
+    var mail = names[i];
+
+    if (mail === names){
+        mailTrovata = false;
+    }
+} 
+/* VERIFICARE MAIL */
+if (mailTrovata){
+    console.log("entra pure");
+} else{
+    console.log("non puoi entrare");
+}
 
 /* MESSAGGIO */
 var message = ["Benvenuti!"];
